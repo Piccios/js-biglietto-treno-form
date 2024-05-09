fe0442643e94120ea22becc8161d3e7c44298803
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-09 19:51:11 +0200

    event listener calculates, data supplied by user, into ticket price

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
 
-// Creo due variabili per la distanza e l'eta' del cliente
-
-const customerTrip = parseInt( ('Quanti chilometri percorrerai?'))
-
-const customerAge = parseInt( ('Quanti anni hai?'))
-
 // Setto il costo del biglietto per km
 
-let ticketPrice = (customerTrip * 0.267113);
+
 
 // Setto uno sconto del 24.552% per i passeggeri con eta' minore di 21 anni e uno sconto del 37.893% per i passeggeri con eta' superiore a 63 anni
 
-if (customerAge < 21) {
-    ticketPrice = ((ticketPrice * 24.552) / 100);
-} else if (customerAge > 63) {
-    ticketPrice = ((ticketPrice * 37.893) / 100);
-}
-
-let ticketPriceRounded = Math.round(ticketPrice * 100) / 100;
-
-console.log(ticketPrice, ticketPriceRounded)
 
 
 
 // creati due input dove l'utente inserira' i suoi dati, creo due variabili con i rispettivi valori
-
 
 
 // Creo un eventListener per il bottone di calcolo del costo
@@ -32,9 +16,25 @@ console.log(ticketPrice, ticketPriceRounded)
 const calcola = document.querySelector('button');
 
 calcola.addEventListener('click', function(){
-    let length = document.getElementById('travelLength')
-    let age = document.getElementById('customerAge')
+    let lengthEl = parseFloat(document.getElementById('travelLength').value);
+    let ageEl = parseFloat(document.getElementById('customerAge').value)
+
+    let ticketPrice = (lengthEl * 0.267113);
+
+    if (ageEl < 21) {
+        ticketPrice = ((ticketPrice * 24.552) / 100);
+    } else if (ageEl > 63) {
+        ticketPrice = ((ticketPrice * 37.893) / 100);
+    }
     
-    console.log('CLICK!', age.value, length.value);
+    let ticketPriceRounded = Math.round(ticketPrice * 100) / 100;
+    
+    console.log(ticketPrice, ticketPriceRounded)
+
+
+
+    console.log('CLICK!', lengthEl, ageEl, ticketPrice + '€', ticketPriceRounded + '€');
+
+    document.getElementById('risultato').innerHTML = ( ticketPrice + '€', ticketPriceRounded + '€')
 })
 
